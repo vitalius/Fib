de313b326a0a1e0e2eaadc232e05329b94b6e8f6
vitaliy <vitaliy@tav.cloudmotion.com>
2026-02-04 12:16:23 -0800

    java library updates

--- src/main/java/com/fib/service/FibResource.java
+++ src/main/java/com/fib/service/FibResource.java
@@ -1,11 +1,10 @@
 package com.fib.service;
 
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import java.math.BigInteger;
 
 @Path("/")
